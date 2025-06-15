2053c75d10d17934971c89fd946cebbf08bae52f
Ynda Jas <yndajas@gmail.com>
2025-06-15 16:30:00 +0100

    Move functions and styles to separate files
    I liked the idea of this being a single file, but I'd like to add some
    new features and it'll be easier to manage with the concerns more
    separated

---
+++ assets/scripts/functions.js
@@ -0,0 +1,81 @@
+import Rgb from "./rgb.js";
+
+/**
+ * @param {string} queryParam The colour query param to check.
+ * @returns {boolean} Whether the query param is valid.
+ */
+function queryParamIsValid(queryParam) {
+  if (!/\d{1,3},\d{1,3},\d{1,3}/.test(queryParam)) return false;
+  return queryParam
+    ?.split(",")
+    .every((component) => Number.parseInt(component, 10) <= 255);
+}
+
+/**
+ * Updates the favicon to a square filled using the given Rgb style.
+ * @param {string} decimalRgbStyle A CSS \<color\> value to use for the
+ * favicon's fill style.
+ */
+function updateFavicon(decimalRgbStyle) {
+  const favicon = new Image();
+  favicon.src = "";
+  const canvas = document.createElement("canvas");
+  canvas.width = 32;
+  canvas.height = 32;
+  const context = canvas.getContext("2d");
+  context.drawImage(favicon, 0, 0);
+  context.fillStyle = decimalRgbStyle;
+  context.fillRect(0, 0, 32, 32);
+  const linkMetaTag =
+    document.querySelector("link[rel*='icon']") ||
+    document.createElement("link");
+  linkMetaTag.type = "image/x-icon";
+  linkMetaTag.rel = "shortcut icon";
+  linkMetaTag.href = canvas.toDataURL("image/x-icon");
+  document.querySelector("head").appendChild(linkMetaTag);
+}
+
+/**
+ * Updates the query param (and adds to the history) without reloading the page.
+ * @param {string} newQueryParam A colour query param.
+ */
+function updateQueryParam(newQueryParam) {
+  const newUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?colour=${newQueryParam}`;
+  window.history.pushState({ path: newUrl }, "", newUrl);
+}
+
+/**
+ * Changes the colour by updating:
+ * - the body's background, the favicon, the displayed CSS \<color\> values, and
+ * the query param to the new colour;
+ * - the text colour to whichever contrasts best out of black or white.
+ * @param {Event} [event=undefined] A triggering event fired by clicking
+ * anywhere other than a button or anchor element.
+ * @param {Rgb} [newRgb=undefined] A specific Rgb to use for the new colour. If
+ * unspecified, a random Rgb will be generated.
+ */
+function changeColour(event = undefined, newRgb = undefined) {
+  if (["A", "BUTTON"].includes(event?.target.nodeName)) return;
+
+  newRgb ||= Rgb.random();
+  document.body.style.backgroundColor = newRgb.cssColorValue();
+  document.body.style.color = newRgb.maxContrastRgb().cssColorValue();
+  document.querySelector("#rgb p").innerText = newRgb.cssColorValue();
+  document.querySelector("#hex p").innerText =
+    newRgb.cssColorValue("hexadecimal");
+  updateFavicon(newRgb.cssColorValue());
+  updateQueryParam(newRgb.queryParam());
+}
+
+/**
+ * Copies to the clipboard the CSS \<color\> value from the sibling p of the
+ * pressed button.
+ * @param {HTMLButtonElement} button The HTMLButtonElement that was clicked on
+ * to request the copy action.
+ */
+function copyCode(button) {
+  const code = button.parentNode.querySelector("p").innerText;
+  navigator.clipboard.writeText(code);
+}
+
+export { changeColour, copyCode, queryParamIsValid, updateFavicon };
---
+++ assets/scripts/rgb.js
@@ -0,0 +1,107 @@
+export default class Rgb {
+  #red;
+  #green;
+  #blue;
+  #_cssColorValues = { decimal: undefined, hexadecimal: undefined };
+  #_luminance;
+
+  /**
+   * @param {number} red The Rgb's red value as an integer from 0 to 255.
+   * @param {number} green The Rgb's green value as an integer from 0 to 255.
+   * @param {number} blue The Rgb's blue value as an integer from 0 to 255.
+   */
+  constructor(red, green, blue) {
+    this.#red = red;
+    this.#green = green;
+    this.#blue = blue;
+  }
+
+  /**
+   * @returns {Rgb}
+   */
+  static random() {
+    return new Rgb(...[...Array(3)].map(this.#randomComponent));
+  }
+
+  /**
+   * @returns {number} An integer between 0 and 255.
+   */
+  static #randomComponent() {
+    return Math.floor(Math.random() * 256);
+  }
+
+  /**
+   * @param {string} [format=decimal] The format of the \<color\> value -
+   * decimal or hexadecimal.
+   * @returns {string} A CSS \<color\> value for the Rgb in the given format.
+   */
+  cssColorValue(format = "decimal") {
+    switch (format) {
+      case "decimal":
+        this.#_cssColorValues.decimal ||= `rgb(${this.#components().join(
+          ", ",
+        )})`;
+        return this.#_cssColorValues.decimal;
+      case "hexadecimal":
+        if (!this.#_cssColorValues.hexadecimal) {
+          const hexComponents = this.#components().map((component) =>
+            component.toString(16).padStart(2, "0"),
+          );
+          this.#_cssColorValues.hexadecimal = `#${hexComponents.join("")}`;
+        }
+        return this.#_cssColorValues.hexadecimal;
+    }
+  }
+
+  /**
+   * @returns {string} The Rgb formatted for the colour query param.
+   */
+  queryParam() {
+    return this.#components().join(",");
+  }
+
+  /**
+   * @returns {Rgb} An Rgb for black or white, whichever contrasts best with the
+   * main colour.
+   */
+  maxContrastRgb() {
+    const blackLuminance = 0.0;
+    const whiteLuminance = 1.0;
+
+    const blackContrast = (this.#luminance() + 0.05) / (blackLuminance + 0.05);
+    const whiteContrast = (whiteLuminance + 0.05) / (this.#luminance() + 0.05);
+
+    return blackContrast > whiteContrast
+      ? new Rgb(0, 0, 0)
+      : new Rgb(255, 255, 255);
+  }
+
+  /**
+   * @returns {number[]} An array of the Rgb's red, green, and blue values as
+   * integers from 0 to 255.
+   */
+  #components() {
+    return [this.#red, this.#green, this.#blue];
+  }
+
+  /**
+   * @returns {number} The Rgb's luminance as a floating point value from 0.0 to
+   * 1.0.
+   */
+  #luminance() {
+    if (!this.#_luminance) {
+      const [rLuminance, gLuminance, bLuminance] = this.#components().map(
+        (component) => {
+          const fractionalComponent = component / 255.0;
+          return fractionalComponent <= 0.04045
+            ? fractionalComponent / 12.92
+            : Math.pow((fractionalComponent + 0.055) / 1.055, 2.4);
+        },
+      );
+      this.#_luminance =
+        0.2126 * rLuminance + 0.7152 * gLuminance + 0.0722 * bLuminance;
+    }
+
+    return this.#_luminance;
+  }
+}
